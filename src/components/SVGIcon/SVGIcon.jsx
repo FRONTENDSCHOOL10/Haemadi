e2aaccc68d17a9adb26f5f4f624d2c67ccdd522b
JoowonJang <wndnjs5248@hanmail.net>
2024-09-02 14:08:12 +0900

    refactor: SVGIcon 리팩토링 및 png 수정
    - sprites.svg의 filter id 수정해서 오류 해결
    - sprites.svg의 조개 아이콘들 모두 png에서 svg로 수정 (피그마 작업 후 export)
    - SVGIcon 컴포넌트의 svg 태그에 viewBox 속성 추가
    - icons.js 파일 생성 및 아이콘 정보 정리

--- src/components/SVGIcon/SVGIcon.jsx
+++ src/components/SVGIcon/SVGIcon.jsx
@@ -6,18 +6,31 @@ SVGIcon.propTypes = {
   name: string.isRequired,
   width: number,
   height: number,
+  color: string,
 };
 
-function SVGIcon({ name, width = 33, height }) {
+function SVGIcon({ name, width = 33, height, color }) {
+  height = height ? height : width;
   return (
-    <svg width={width} height={height ? height : width}>
-      <use
-        width={width}
-        height={height ? height : width}
-        href={`${icons}#${name}`}
-      />
+    <svg
+      width={width}
+      height={height}
+      viewBox={`0 0 ${width} ${height}`}
+      style={{ color }}
+    >
+      <use width={width} height={height} href={`${icons}#${name}`} />
     </svg>
   );
 }
+
+/*
+사용 예시
+<SVGIcon
+  name={icons.shell_tired_block.name}
+  width={icons.shell_tired_block.width}
+  height={icons.shell_tired_block.height}
+  color={icons.shell_tired_block.color}
+/>
+*/
 
 export default SVGIcon;
