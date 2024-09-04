f72ec0f690d22b44286e7906c93037ac8846edf0
jaehwan-space <ma2582m@naver.com>
2024-09-04 13:22:48 +0900

    refactor: CSS 변수 적용

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,13 @@ import '@/styles/main.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import Button from './components/Button/Button';
 
 const container = document.getElementById('root');
-
-const onClick = () => console.log('안녕');
 
 if (container) {
   createRoot(container).render(
     <StrictMode>
       <App />
-      <Button type="normal" state="disabled" onClick={onClick}>
-        가입
-      </Button>
     </StrictMode>
   );
 } else {
