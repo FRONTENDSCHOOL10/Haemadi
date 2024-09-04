a0a7e3a6a39b37f683a57eb2ca5a07ab339a73c6
JoowonJang <wndnjs5248@hanmail.net>
2024-09-04 22:15:59 +0900

    feat: PlusButton 컴포넌트 제작
    - useMediaQuery 훅을 사용해서 DESKTOP(가로 640px) 기준으로 icon 변경
    - Framer motion을 사용하기 위해 forwardRef를 사용했음
    - 개발자 도구 탭에서 Component의 이름을 헷갈리지 않게 displayName을 PlusButton으로 설정

---
+++ src/pages/home/components/PlusButton/PlusButton.jsx
@@ -0,0 +1,26 @@
+import SVGIcon from '@/components/SVGIcon/SVGIcon';
+import { DESKTOP } from '@/constants';
+import icons from '@/icons';
+import { func } from 'prop-types';
+import { forwardRef } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import style from './PlusButton.module.css';
+
+const PlusButton = forwardRef(function _PlusButton({ onClick }, ref) {
+  const desktop = useMediaQuery(DESKTOP);
+  const plusIcon = icons[`plus${desktop ? '_pc' : ''}`];
+
+  return (
+    <button ref={ref} className={style.plusButton} onClick={onClick}>
+      <SVGIcon {...plusIcon} />
+    </button>
+  );
+});
+
+PlusButton.propTypes = {
+  onClick: func,
+};
+
+PlusButton.displayName = 'PlusButton';
+
+export default PlusButton;
