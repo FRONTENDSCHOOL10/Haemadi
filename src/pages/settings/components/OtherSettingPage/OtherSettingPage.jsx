ec1554c66a89fdde7505f65aa3a71b73ea59d167
jaehwan-space <ma2582m@naver.com>
2024-09-05 10:21:17 +0900

    feat: OtherSettingPage 컴포넌트 제작

---
+++ src/pages/settings/components/OtherSettingPage/OtherSettingPage.jsx
@@ -0,0 +1,54 @@
+import { memo } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { number, string, oneOf, oneOfType } from 'prop-types';
+
+import styles from './OtherSettingPage.module.css';
+import icons from '@/icons';
+import SVGIcon from '@/components/SVGIcon/SVGIcon';
+
+OtherSettingPage.propTypes = {
+  type: oneOf([
+    'announcement',
+    'headset',
+    'question',
+    'list',
+    'information',
+    'version',
+  ]),
+  navigateTo: oneOfType([number, string]),
+};
+
+const OtherSettingPageList = {
+  announcement: '공지사항',
+  headset: '문의하기',
+  question: '자주 묻는 질문',
+  list: '이용약관',
+  information: '개인정보 처리방침',
+  version: '버전정보(1.00)',
+};
+
+function OtherSettingPage({ type = 'announcement', navigateTo = -1 }) {
+  const navigate = useNavigate();
+
+  const iconStyle = icons[type];
+
+  const handleNavigation = () => {
+    navigate(navigateTo); // props로 받은 navigateTo 값으로 이동
+  };
+
+  return (
+    <button
+      className={styles.settingButton}
+      type="button"
+      onClick={handleNavigation}
+    >
+      <div className={styles.settingTitle}>
+        <SVGIcon {...iconStyle} />
+        <span>{OtherSettingPageList[type]}</span>
+      </div>
+      <SVGIcon {...icons.rightDir} />
+    </button>
+  );
+}
+
+export default memo(OtherSettingPage);
