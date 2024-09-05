eba79e72c57bad80272e17495fe0c093d53499e9
JoowonJang <wndnjs5248@hanmail.net>
2024-09-05 17:11:59 +0900

    refactor: PlusButton 컴포넌트 memo사용
    - memo 사용
    - 주석 추가

--- src/pages/home/components/PlusButton/PlusButton.jsx
+++ src/pages/home/components/PlusButton/PlusButton.jsx
@@ -2,10 +2,12 @@ import SVGIcon from '@/components/SVGIcon/SVGIcon';
 import { DESKTOP } from '@/constants';
 import icons from '@/icons';
 import { func } from 'prop-types';
-import { forwardRef } from 'react';
+import { forwardRef, memo } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import style from './PlusButton.module.css';
 
+// 부모의 상태와 관련된 애니메이션으로 framer-motion 사용을 위해 forwardRef 사용
+// export 부분에서 forwardRef 안쓰고 이렇게 쓰니까 propTypes 사용 가능
 const PlusButton = forwardRef(function _PlusButton({ onClick }, ref) {
   const desktop = useMediaQuery(DESKTOP);
   const plusIcon = icons[`plus${desktop ? '_pc' : ''}`];
@@ -27,6 +29,7 @@ PlusButton.propTypes = {
   onClick: func,
 };
 
+// 개발자 도구 탭의 Components에 표시될 이름 (안쓰면 _PlusButton으로 나옴)
 PlusButton.displayName = 'PlusButton';
 
-export default PlusButton;
+export default memo(PlusButton);
