7f6179bf3983f672cd744727401406ef3dfddf59
jaehwan-space <ma2582m@naver.com>
2024-09-06 16:14:07 +0900

    feat: InterestBadge 컴포넌트 및 폴더 기본 설정

---
+++ src/pages/myPage/components/InterestBadge/InterestBadge.jsx
@@ -0,0 +1,14 @@
+import { memo } from 'react';
+import styles from './InterestBadge.module.css';
+
+
+function InterestBadge({list}) {
+
+  return (
+   {list.map((element)=>{
+
+   })}
+  );
+}s
+
+export default memo(InterestBadge);
