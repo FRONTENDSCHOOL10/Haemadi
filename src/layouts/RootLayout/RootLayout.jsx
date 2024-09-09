6881588306454fc976991b1a92ac8fc72e610fde
JoowonJang <wndnjs5248@hanmail.net>
2024-09-10 00:52:53 +0900

    feat: router 구성 및 GlobalNav 조건부 렌더링
    - GlobalNav를 사용하는 페이지는 모두 RootLayout의 children으로 설정
    - home, my 두 페이지에서만 GlobalNav를 모바일에서도 렌더링하도록 설정

--- src/layouts/RootLayout/RootLayout.jsx
+++ src/layouts/RootLayout/RootLayout.jsx
@@ -1,10 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { useMediaStore } from '@/stores/mediaStore';
+import { Outlet, useLocation } from 'react-router-dom';
+import GlobalNav from '../GlobalNav/GlobalNav';
 import style from './RootLayout.module.css';
 
 function RootLayout() {
+  const { pathname } = useLocation();
+  const desktop = useMediaStore((store) => store.desktop);
+  const renderMobileNav = pathname === '/' || pathname === '/my';
+
   return (
     <div className={style.component}>
       <Outlet />
+      {/* GlobalNav는 home과 my 페이지에서만 mobile에서도 렌더링, 나머지는 desktop에서만 렌더링 */}
+      {renderMobileNav ? <GlobalNav /> : desktop && <GlobalNav />}
     </div>
   );
 }
