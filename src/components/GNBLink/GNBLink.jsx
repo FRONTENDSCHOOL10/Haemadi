96d7e4e6bdc91342d3dea0e330c874efe3fe6fd2
siyo0723 <junior0723@naver.com>
2024-09-09 16:48:05 +0900

    style: 중단점 useMediaQuery -> useMideaStore 변경

--- src/components/GNBLink/GNBLink.jsx
+++ src/components/GNBLink/GNBLink.jsx
@@ -3,7 +3,7 @@ import icons from '@/icons';
 import { memo } from 'react';
 import { NavLink } from 'react-router-dom';
 import SVGIcon from '../SVGIcon/SVGIcon';
-import { useMediaQuery } from 'react-responsive';
+import { useMideaStore } from 'react-responsive';
 import PropTypes from 'prop-types'; 
 
 GNBLink.propTypes = {
@@ -11,7 +11,7 @@ GNBLink.propTypes = {
 };
 
 function GNBLink({ darkBg = false }) {
-  const desktop = useMediaQuery({ query: '(min-width: 640px)' });
+  const desktop = useMideaStore(store => store.desktop);
 
   const navItems = [
     {
