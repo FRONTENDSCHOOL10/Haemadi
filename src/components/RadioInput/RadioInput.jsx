96037258a44fad44a11b022d0ba7a4cc6a0c8d47
jaehwan-space <ma2582m@naver.com>
2024-09-10 17:51:45 +0900

    refactor: groupName prop 삭제 - type prop에 experience 추가함

--- src/components/RadioInput/RadioInput.jsx
+++ src/components/RadioInput/RadioInput.jsx
@@ -1,18 +1,17 @@
 import { memo, useId } from 'react';
-import { node, oneOf, string, func } from 'prop-types';
+import { node, oneOf, func } from 'prop-types';
 
 import styles from './RadioInput.module.css';
 
 // props type 정의
 RadioInput.propTypes = {
   children: node.isRequired,
-  groupName: string.isRequired,
-  type: oneOf(['gender', 'age', 'emotion']),
+  type: oneOf(['gender', 'age', 'emotion', 'experience']),
   onChange: func,
 };
 
 // RadioInput Component
-function RadioInput({ children, groupName, type = 'emotion', onChange }) {
+function RadioInput({ children, type = 'emotion', onChange }) {
   const radioInputId = useId();
   const classNames = `${styles.radioStyles} ${styles[type]}`.trim();
 
@@ -23,7 +22,7 @@ function RadioInput({ children, groupName, type = 'emotion', onChange }) {
       <input
         className={styles['appearance-none']}
         type="radio"
-        name={groupName}
+        name={type}
         value={children}
         id={radioInputId}
         onChange={handleChange}
