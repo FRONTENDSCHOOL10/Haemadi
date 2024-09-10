000f64e3ab2f9121569aab947b15a8f985475f69
jaehwan-space <ma2582m@naver.com>
2024-09-10 11:17:23 +0900

    feat: RadioInput 컴포넌트 제작 - 3가지 스타일

---
+++ src/components/RadioInput/RadioInput.jsx
@@ -0,0 +1,42 @@
+import { memo, useId } from 'react';
+import { node, oneOf, string, func } from 'prop-types';
+
+import styles from './RadioInput.module.css';
+
+// props type 정의
+RadioInput.propTypes = {
+  children: node.isRequired,
+  groupName: string.isRequired,
+  type: oneOf(['gender', 'age', 'emotion']),
+  onChange: func,
+};
+
+// RadioInput Component
+function RadioInput({ children, groupName, type = 'emotion', onChange }) {
+  const radioInputId = useId();
+  const classNames = `${styles[type]}`;
+
+  const handleChange = () => {
+    if (onChange) {
+      onChange(children);
+    }
+  };
+
+  return (
+    <>
+      <input
+        className={styles['appearance-none']}
+        type="radio"
+        name={groupName}
+        value={children}
+        id={radioInputId}
+        onChange={handleChange}
+      />
+      <label className={classNames} htmlFor={radioInputId}>
+        {children}
+      </label>
+    </>
+  );
+}
+
+export default memo(RadioInput);
