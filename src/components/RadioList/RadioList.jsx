a221fc9b4a22f2fc029c81d850320886fde55cb1
jaehwan-space <ma2582m@naver.com>
2024-09-10 22:13:33 +0900

    feat: RadioList 컴포넌트 제작 - AgeRadioList 추가 - type prop에 따라 컴포넌트 달라짐

--- src/components/RadioList/RadioList.jsx
+++ src/components/RadioList/RadioList.jsx
@@ -1,30 +1,50 @@
 import { useState, useCallback } from 'react';
+import { oneOf } from 'prop-types';
 
 import styles from './RadioList.module.css';
 import RadioInput from '@/components/RadioInput/RadioInput';
 
-const emotion = [
-  '행복해요',
-  '평범해요',
-  '슬퍼요',
-  '피곤해요',
-  '꿀꿀해요',
-  '힘들어요',
-  '모르겠어요',
-];
+const RADIO_VALUES = {
+  emotion: [
+    '행복해요',
+    '평범해요',
+    '슬퍼요',
+    '피곤해요',
+    '꿀꿀해요',
+    '힘들어요',
+    '모르겠어요',
+  ],
+  age: [
+    '9세 이하',
+    '10~14세',
+    '15~19세',
+    '20~24세',
+    '25~30세',
+    '31세 이상',
+    '비공개',
+  ],
+};
 
-function RadioList() {
+RadioList.propTypes = {
+  type: oneOf(['gender', 'age', 'emotion', 'experience']).isRequired,
+};
+
+function RadioList({ type }) {
   const [selected, setSelected] = useState('');
+
+  const classNames = `${styles.radioList} ${styles[type + 'Wrapper']}`;
 
   const handleChange = useCallback((value) => {
     setSelected(value);
   }, []);
 
   return (
-    <ul className={styles.emotionWrapper}>
-      {emotion.map((element) => (
+    <ul className={classNames}>
+      {RADIO_VALUES[type].map((element) => (
         <li key={element}>
-          <RadioInput onChange={handleChange}>{element}</RadioInput>
+          <RadioInput type={type} onChange={handleChange}>
+            {element}
+          </RadioInput>
         </li>
       ))}
     </ul>
