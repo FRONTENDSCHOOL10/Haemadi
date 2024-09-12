a05594c40b9fd3d381348bd1e8e94a5b4e81a3f4
jaehwan-space <ma2582m@naver.com>
2024-09-13 01:37:47 +0900

    feat: myPage 구성 - 하단 menu 제작

--- src/pages/myPage/index.jsx
+++ src/pages/myPage/index.jsx
@@ -1,5 +1,32 @@
+import { Link } from 'react-router-dom';
+
+import styles from './myPage.module.css';
+import SVGIcon from '@/components/SVGIcon/SVGIcon';
+import InterestBadge from './components/InterestBadge/InterestBadge';
+import icons from '@/icons';
+import LinkButton from './components/LinkButton/LinkButton';
+
 function MyPage() {
-  return <></>;
+  return (
+    <div className={styles.myPage}>
+      <h1 className="sr-only">마이페이지</h1>
+      <div className={styles.userProfile}>
+        <Link className={styles.setting} to="./settings" aria-label="설정">
+          <SVGIcon {...icons.setting} />
+        </Link>
+      </div>
+      <div className={styles.menu}>
+        <div className={styles.LinkWrapper}>
+          <LinkButton type="bottles">나의 기록</LinkButton>
+          <LinkButton type="statistics">나의 섬 통계</LinkButton>
+        </div>
+        <div className={styles.interestWrapper}>
+          <h2>관심사</h2>
+          <InterestBadge />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyPage;
