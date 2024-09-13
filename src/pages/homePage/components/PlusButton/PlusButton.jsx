5425ddff15d9fe992ac2fa6dcc0cec9118d50fee
JoowonJang <wndnjs5248@hanmail.net>
2024-09-14 00:39:45 +0900

    refactor: PlusButton 컴포넌트의 id 값 고유하게 수정
    - useId 사용
    - 주석 추가

--- src/pages/homePage/components/PlusButton/PlusButton.jsx
+++ src/pages/homePage/components/PlusButton/PlusButton.jsx
@@ -4,6 +4,7 @@ import { useMediaStore } from '@/stores/mediaStore';
 import { bool, func } from 'prop-types';
 import { forwardRef, memo } from 'react';
 import styles from './PlusButton.module.css';
+import { useId } from 'react';
 
 // 부모의 상태와 관련된 애니메이션으로 framer-motion 사용을 위해 forwardRef 사용
 // forwardRef를 export 부분에서 안쓰고 함수 선언부를 감싸니까 propTypes 사용 가능
@@ -13,14 +14,15 @@ const PlusButton = forwardRef(function _PlusButton(
 ) {
   const desktop = useMediaStore((store) => store.desktop);
   const plusIcon = icons[`plus${desktop ? '_pc' : ''}`];
-  const label = activated ? '감정 선택 취소' : '일기 쓰기';
+  const label = activated ? '감정 선택 취소' : '일기 쓰기'; // 클릭 상태에 따라 aria-label 변경
+  const buttonLabelId = useId();
 
   return (
     <>
       <button
         type="button"
-        title={(!desktop || activated) && label}
-        aria-labelledby="buttonLabel"
+        title={label}
+        aria-labelledby={buttonLabelId}
         ref={ref}
         className={styles.plusButton}
         onClick={onClick}
@@ -29,7 +31,8 @@ const PlusButton = forwardRef(function _PlusButton(
         <SVGIcon {...plusIcon} />
       </button>
       <span
-        id="buttonLabel"
+        id={buttonLabelId}
+        // 모바일에서와 activated일 때 숨김 (sr-only)
         className={`${styles.buttonLabel}${!desktop || activated ? ' sr-only' : ''}`}
       >
         {label}
