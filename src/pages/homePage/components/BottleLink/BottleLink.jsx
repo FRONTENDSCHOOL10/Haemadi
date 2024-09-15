e5959c374181a0f782ab3f60fd1802608f90f68e
JoowonJang <wndnjs5248@hanmail.net>
2024-09-15 22:39:30 +0900

    refactor: BottleLink 컴포넌트 리팩토링
    - 반응형 디자인추가 및 수정
    - 위 쪽에 메세지가 떠있지 않을 때는 클릭할 수 없도록 해야 하므로 button으로 변경
    - type prop 및 이미지 이름을 mailBox -> letterBox로 router의 주소와 동일하게 변경

--- src/pages/homePage/components/BottleLink/BottleLink.jsx
+++ src/pages/homePage/components/BottleLink/BottleLink.jsx
@@ -1,60 +1,66 @@
-import { useMediaStore } from '@/stores/mediaStore';
-import { oneOf } from 'prop-types';
-import { Link } from 'react-router-dom';
+import { bool, oneOf, string } from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import styles from './BottleLink.module.css';
-import bottleLink_mailBox from '/homePage/bottleLink_mailBox.png';
+import bottleLink_letterBox from '/homePage/bottleLink_letterBox.png';
 import bottleLink_pickUp from '/homePage/bottleLink_pickUp.png';
 
 BottleLink.propTypes = {
-  type: oneOf(['pickUpBottle', 'mailBox']).isRequired,
+  type: oneOf(['pickUpBottle', 'letterBox']).isRequired,
+  disabled: bool,
+  className: string,
 };
 
-function BottleLink({ type }) {
-  const desktop = useMediaStore((store) => store.desktop);
+const typeConfigs = {
+  pickUpBottle: {
+    navigateTo: '/pick-up-bottle',
+    linkLabel: '유리병 건지기 (답장할 일기 선택)',
+    bottleImg: bottleLink_pickUp,
+    cssText: { width: 'calc(115px + 3.5vw)' },
+    notificationMessage: '누군가 당신의 답장을\n기다리고 있어요!',
+  },
+  letterBox: {
+    navigateTo: '/letter-box',
+    linkLabel: '유리병 편지함 (받은 답장 확인)',
+    bottleImg: bottleLink_letterBox,
+    cssText: { width: 'calc(150px + 6vw)' },
+    notificationMessage: '바다 건너에서 떠내려온\n편지를 확인해 보세요!',
+  },
+};
 
-  const typeConfigs = {
-    pickUpBottle: {
-      linkTo: '/pick-up-bottle',
-      linkLabel: '유리병 건지기 (답장할 일기 선택)',
-      bottleImg: bottleLink_pickUp,
-      desktopSize: { width: '182px', height: '102px' },
-      mobileSize: { width: '130px', height: '73px' },
-    },
-    mailBox: {
-      linkTo: '/mail-box',
-      linkLabel: '유리병 편지함 (받은 답장 확인)',
-      bottleImg: bottleLink_mailBox,
-      desktopSize: { width: '283px', height: '172px' },
-      mobileSize: { width: '171px', height: '104px' },
-    },
-  };
+function BottleLink({ type, disabled = false, className }) {
+  const navigate = useNavigate();
+  const classNames = `${styles.componentWrapper} ${className}`.trim();
 
-  const { linkTo, linkLabel, bottleImg, desktopSize, mobileSize } =
-    typeConfigs[type];
+  const {
+    navigateTo,
+    linkLabel,
+    bottleImg,
+    cssText,
 
-  const imgStyle = desktop ? desktopSize : mobileSize;
+    notificationMessage,
+  } = typeConfigs[type];
+
+  const handleBottleClick = () => navigate(navigateTo);
 
   return (
-    <>
-      {/* 답장이 왔을 때만 말풍선 표시하는 건 나중에 서버에 read 필드를 추가하거나 해야 할 듯 */}
-      {type === 'mailBox' && (
+    <div className={classNames}>
+      {!disabled && (
         <div className={styles.notification} role="alert">
-          바다 건너에서 떠내려온
-          <br />
-          편지를 확인해 보세요!
+          {notificationMessage}
         </div>
       )}
-      <Link
-        to={linkTo}
+      <button
+        type="button"
         className={styles.bottleLink}
-        role="button"
         aria-label={`편지가 든 유리병, ${linkLabel} 페이지로 이동`}
         title={`${linkLabel} 페이지로 이동`}
+        onClick={handleBottleClick}
+        disabled={disabled}
       >
         {/* 스크린 리더가 이미지를 무시하도록 alt 속성 일부러 비워둠. Link 태그의 aria-label 속성으로 설명함. */}
-        <img src={bottleImg} alt="" style={imgStyle} />
-      </Link>
-    </>
+        <img src={bottleImg} alt="" style={cssText} />
+      </button>
+    </div>
   );
 }
 
