4884531a6acc8a7435b3dafe4075baea7cfd21ed
JoowonJang <wndnjs5248@hanmail.net>
2024-09-16 17:39:37 +0900

    refactor: demoPage의 StepIndicator 컴포넌트 propTypes 수정
    - useParams로 받아오는 숫자가 문자열이기 때문에 '1', '2', '3'으로 변경

--- src/pages/demoPage/components/StepIndicator/StepIndicator.jsx
+++ src/pages/demoPage/components/StepIndicator/StepIndicator.jsx
@@ -2,7 +2,7 @@ import { oneOf } from 'prop-types';
 import { memo } from 'react';
 import styles from './StepIndicator.module.css';
 
-const STEP_RANGE = [1, 2, 3];
+const STEP_RANGE = ['1', '2', '3'];
 
 StepIndicator.propTypes = {
   step: oneOf(STEP_RANGE).isRequired,
