ee576136f0ea86d4eb636de66a5d306920d2bf5a
JoowonJang <wndnjs5248@hanmail.net>
2024-09-18 02:08:28 +0900

    comment: 주석 제거

--- src/pages/splashPage/index.jsx
+++ src/pages/splashPage/index.jsx
@@ -7,7 +7,7 @@ function SplashPage() {
     <div className={styles.splashPage}>
       <span className="sr-only">Splash Screen</span>
     </div>,
-    portal // document.body 또는 다른 DOM 노드로 포탈 렌더링
+    portal
   );
 }
 
