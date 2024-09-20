d68e70a7b4ee1e030b67e8f72ebdfafeef3ab357
jaehwan-space <ma2582m@naver.com>
2024-09-20 17:53:02 +0900

    refactor: 로컬스토리지 사용하여 유저 정보 가져옴

--- src/pages/myPage/components/UserProfile/UserProfile.jsx
+++ src/pages/myPage/components/UserProfile/UserProfile.jsx
@@ -4,41 +4,35 @@ import { SyncLoader } from 'react-spinners';
 import styles from './UserProfile.module.css';
 import defaultProfile from '@/assets/default_Profile.png';
 import { getStorage } from '@/utils';
-import { getUserData, getUserProfileImg } from '@/api/users';
+import { getUserProfileImg } from '@/api/users';
 
 function UserProfile() {
-  /* 추후 유저 전역 상태 관리 제작 시 변경 */
-  const [userData, setUserData] = useState(null); // 유저 데이터를 저장할 상태
   const [loading, setLoading] = useState(true); // 로딩 상태 관리
-  const USER_TOKEN = getStorage('token');
+  const [userData, setUserData] = useState(null); // 유저 데이터를 상태로 관리
 
   useEffect(() => {
-    // 토큰이 존재하고 유저 데이터가 없을 경우에만 API 호출
-    if (USER_TOKEN && !userData) {
-      getUserData(USER_TOKEN)
-        .then((data) => {
-          setUserData(data); // 가져온 유저 데이터를 상태에 저장
-        })
-        .catch((error) => {
-          console.error(
-            '유저 데이터를 불러오는 중 오류가 발생했습니다.',
-            error
-          );
-        })
-        .finally(() => setLoading(false)); // 성공 여부와 상관없이 로딩 종료
+    // 로컬 스토리지에서 authStore의 유저 정보 불러오기
+    const authStore = getStorage('authStore');
+    const storedUserData = authStore?.state?.userInfo || null;
+
+    // 만약 유저 정보가 있다면 상태에 저장하고, 로딩을 종료함
+    if (storedUserData) {
+      setUserData(storedUserData);
+      setLoading(false);
     } else {
-      setLoading(false); // 이미 데이터가 존재하면 로딩 종료
+      setLoading(false); // 유저 정보가 없을 경우 로딩만 종료
     }
-  }, [USER_TOKEN, userData]);
+  }, []);
 
   if (loading) {
     return <SyncLoader color="#2E7FB9" />; // 로딩 상태를 보여줌
   }
 
-  // 데이터가 없는 경우 처리 (추후에 빈 화면 처리 가능)
+  // 데이터가 없는 경우 처리
   if (!userData) {
     return <p>유저 데이터를 불러올 수 없습니다.</p>;
   }
+  console.log(userData);
 
   return (
     <>
