fd7e4ebc375e9d9497099d61ff1a5d08d1b9e623
JoowonJang <wndnjs5248@hanmail.net>
2024-09-21 18:29:12 +0900

    refactor: getRandomNumbers 유틸 함수 리팩토링
    - length가 want보다 작을 때, 0이상 length미만의 수가 최소 하나씩 포함되도록 함

--- src/utils/getRandomNumbers.js
+++ src/utils/getRandomNumbers.js
@@ -1,11 +1,18 @@
+/** @type {(length: number, want: number) => Array<number>} */
 export function getRandomNumbers(length, want = 5) {
   let result = [];
   let numbers = Array.from({ length: length }, (_, i) => i);
 
   if (length < want) {
-    // 중복 허용
-    for (let i = 0; i < want; i++) {
-      result.push(numbers[Math.floor(Math.random() * length)]);
+    // 먼저 모든 숫자를 배열에 넣고 섞음
+    while (numbers.length) {
+      let randomIndex = Math.floor(Math.random() * numbers.length);
+      result.push(numbers[randomIndex]);
+      numbers.splice(randomIndex, 1);
+    }
+    // 추가로 필요한 숫자를 무작위로 선택해 채움
+    while (result.length < want) {
+      result.push(Math.floor(Math.random() * length));
     }
   } else {
     // 중복 허용하지 않음
