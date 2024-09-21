7c26b53b0da93b4b54c8f02e63fce62043cc6849
siyo0723 <junior0723@naver.com>
2024-09-22 02:46:32 +0900

    design:settingsPage 반응형 고려 css 수정,테스트 및 웹 표준,접근성 수정

--- src/pages/settingsPage/index.jsx
+++ src/pages/settingsPage/index.jsx
@@ -1,17 +1,31 @@
-import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import styles from './settingsPage.module.css';
-import SVGIcon from '@/components/SVGIcon/SVGIcon';
-import icons from '@/icons';
+import BackButton from '@/components/BackButton/BackButton';
 import { useMediaStore } from '@/stores/mediaStore';
 import ProfileSettingInfo from './components/ProfileSettingInfo/ProfileSettingInfo';
 import OtherSettingList from './components/OtherSettingList/OtherSettingList';
 
 function SettingsPage() {
+  const desktop = useMediaStore((store) => store.desktop);
+  const SmallScreen = useMediaQuery({ query: '(max-width: 400px)' });
+
+  const topPosition = SmallScreen ? '29px' : '68px'; // 400px 이하일 때 top 조절
+
   return (
-    <>
+    <div className={styles.settingsPage}>
+      <h1>설정</h1>
+      <BackButton
+        color="white"
+        aria-label="뒤로가기"
+        style={{
+          position: 'absolute',
+          top: topPosition,
+          left: desktop ? '43vw' : '7vw',
+        }}
+      />
       <ProfileSettingInfo />
       <OtherSettingList />
-    </>
+    </div>
   );
 }
 
