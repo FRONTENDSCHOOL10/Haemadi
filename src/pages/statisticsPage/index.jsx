e0f12d3c8fab9f8d6e66d4192755836ffeb27fe5
jaehwan-space <ma2582m@naver.com>
2024-09-22 05:44:36 +0900

    feat: staticsPage 제작

--- src/pages/statisticsPage/index.jsx
+++ src/pages/statisticsPage/index.jsx
@@ -1,5 +1,25 @@
+import styles from './statisticsPage.module.css';
+import BackButton from '@/components/BackButton/BackButton';
+import EmotionAverage from './components/EmotionPercentage/EmotionPercentage';
+import ReplyFromPercentage from './components/ReplyFromPercentage/ReplyFromPercentage';
+import AnalysisReport from './components/AnalysisReport/AnalysisReport';
+import AIReplyType from './components/AIReplyType/AIReplyType';
+
 function StatisticsPage() {
-  return <></>;
+  return (
+    <div className={styles.StatisticsPage}>
+      <div className={styles.backButton}>
+        <BackButton />
+      </div>
+      <h1 className={styles.title}>나의 섬 통계 </h1>
+      <div className={styles.cardWrapper}>
+        <EmotionAverage />
+        <ReplyFromPercentage />
+        <AIReplyType />
+        <AnalysisReport />
+      </div>
+    </div>
+  );
 }
 
 export default StatisticsPage;
