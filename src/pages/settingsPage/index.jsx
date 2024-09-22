21e3d109f6256edb8d80672215d307fa73c8c1b4
siyo0723 <junior0723@naver.com>
2024-09-22 17:37:28 +0900

    refactor:settingsPage memo추가, loading컴포 제거

--- src/pages/settingsPage/index.jsx
+++ src/pages/settingsPage/index.jsx
@@ -1,29 +1,16 @@
-import Loading from '@/components/Loading/Loading';
-import { useMediaQuery } from 'react-responsive';
-import styles from './settingsPage.module.css';
 import BackButton from '@/components/BackButton/BackButton';
 import { useMediaStore } from '@/stores/mediaStore';
-import ProfileSettingInfo from './components/ProfileSettingInfo/ProfileSettingInfo';
+import { memo } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import OtherSettingList from './components/OtherSettingList/OtherSettingList';
-import { useState, useEffect } from 'react'; 
+import ProfileSettingInfo from './components/ProfileSettingInfo/ProfileSettingInfo';
+import styles from './settingsPage.module.css';
 
 function SettingsPage() {
-  const [loading, setLoading] = useState(true);
   const desktop = useMediaStore((store) => store.desktop);
   const SmallScreen = useMediaQuery({ query: '(max-width: 400px)' });
 
   const topPosition = SmallScreen ? '29px' : '68px'; // 400px 이하일 때 top 조절
-  useEffect(() => {
-    const simulateLoading = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(simulateLoading); // 컴포넌트 언마운트 시 타이머 정리
-  }, []);
-
-  if (loading) {
-    return <Loading />;
-  }
 
   return (
     <div className={styles.settingsPage}>
@@ -43,4 +30,4 @@ function SettingsPage() {
   );
 }
 
-export default SettingsPage;
+export default memo(SettingsPage);
