708407f077ecd81bdd02bac850236c5389d6b504
siyo0723 <junior0723@naver.com>
2024-09-22 22:19:13 +0900

    refactor:useMediaQuery제거, max-width 400px->440px 병합 개선

--- src/pages/settingsPage/index.jsx
+++ src/pages/settingsPage/index.jsx
@@ -1,16 +1,12 @@
 import BackButton from '@/components/BackButton/BackButton';
 import { useMediaStore } from '@/stores/mediaStore';
 import { memo } from 'react';
-import { useMediaQuery } from 'react-responsive';
 import OtherSettingList from './components/OtherSettingList/OtherSettingList';
 import ProfileSettingInfo from './components/ProfileSettingInfo/ProfileSettingInfo';
 import styles from './SettingsPage.module.css';
 
 function SettingsPage() {
   const desktop = useMediaStore((store) => store.desktop);
-  const SmallScreen = useMediaQuery({ query: '(max-width: 400px)' });
-
-  const topPosition = SmallScreen ? '29px' : '68px'; // 400px 이하일 때 top 조절
 
   return (
     <div className={styles.settingsPage}>
@@ -18,11 +14,7 @@ function SettingsPage() {
       <BackButton
         color="white"
         aria-label="뒤로가기"
-        style={{
-          position: 'absolute',
-          top: topPosition,
-          left: desktop ? '43vw' : '7vw',
-        }}
+        className={`${styles.backButton} ${desktop ? styles.desktopBackButton : styles.mobileBackButton}`}
       />
       <ProfileSettingInfo />
       <OtherSettingList />
