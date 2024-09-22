f6e9ce1e18872739ebb4f20719564f02f3c4d694
jaehwan-space <ma2582m@naver.com>
2024-09-23 01:52:10 +0900

    feat: 통계페이지 userID 연결

--- src/pages/statisticsPage/index.jsx
+++ src/pages/statisticsPage/index.jsx
@@ -1,21 +1,23 @@
 import styles from './statisticsPage.module.css';
+import { BASE_URL } from '@/api/pbconfig';
+import { useAuthStore } from '@/stores/authStore';
+import useFetch from '@/hooks/useFetch';
 import Loading from '@/components/Loading/Loading';
 import BackButton from '@/components/BackButton/BackButton';
 import EmotionAverage from './components/EmotionPercentage/EmotionPercentage';
 import ReplyFromPercentage from './components/ReplyFromPercentage/ReplyFromPercentage';
 import AnalysisReport from './components/AnalysisReport/AnalysisReport';
 import AIReplyType from './components/AIReplyType/AIReplyType';
-import { BASE_URL } from '@/api/pbconfig';
-import useFetch from '@/hooks/useFetch';
 
 function StatisticsPage() {
+  const userInfo = useAuthStore((store) => store.userInfo);
   const currentDate = new Date();
   const year = currentDate.getFullYear();
   const month = String(currentDate.getMonth() + 1).padStart(2, '0');
   const startDate = `${year}-${month}-01`;
 
   /* 이번달 작성한 일기, 답장 가져옴 */
-  const filterQuery = `(userId='nxorcbf2dujhxfu' && created>='${startDate}')`;
+  const filterQuery = `(userId='${userInfo.id}' && created>='${startDate}')`;
   const DIARY_ENDPOINT = `${BASE_URL}/api/collections/diaries/records?sort=created&filter=${encodeURIComponent(filterQuery)}`;
   const REPLY_ENDPOINT = `${BASE_URL}/api/collections/replies/records?sort=created&filter=${encodeURIComponent(filterQuery)}`;
   const {
