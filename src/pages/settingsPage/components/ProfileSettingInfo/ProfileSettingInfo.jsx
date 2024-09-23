f02cb7c5f77e7f3b180d4d64c8d01084ca7755e3
jaehwan-space <ma2582m@naver.com>
2024-09-23 15:36:03 +0900

    refactor: 이미지 경로 변경 수정

--- src/pages/settingsPage/components/ProfileSettingInfo/ProfileSettingInfo.jsx
+++ src/pages/settingsPage/components/ProfileSettingInfo/ProfileSettingInfo.jsx
@@ -1,5 +1,5 @@
 import { getUserProfileImg, updateUserProfileImage } from '@/api/users';
-import defaultProfile from '@/assets/default_profile.png';
+import defaultProfile from '/default_Profile.webp';
 import SVGIcon from '@/components/SVGIcon/SVGIcon';
 import icons from '@/icons';
 import { useAuthStore } from '@/stores/authStore';
@@ -12,7 +12,9 @@ function ProfileSettingInfo() {
   const fileInput = useRef(null);
   const userInfo = useAuthStore((store) => store.userInfo);
 
-  const [image, setImage] = useState(userInfo.profileImage ? getUserProfileImg(userInfo) : defaultProfile);
+  const [image, setImage] = useState(
+    userInfo.profileImage ? getUserProfileImg(userInfo) : defaultProfile
+  );
 
   const onChange = async (e) => {
     if (e.target.files[0]) {
@@ -40,7 +42,7 @@ function ProfileSettingInfo() {
   return (
     <div className={styles.profileContainer}>
       <h2>프로필 설정</h2>
-      
+
       <input
         id="profileImgInput"
         className={`sr-only ${styles.profileImgInput}`}
@@ -56,29 +58,42 @@ function ProfileSettingInfo() {
           src={image}
           alt={`${userInfo.nickName}의 프로필 이미지`}
         />
-        <SVGIcon className={styles.svgCamera} {...icons.camera}  width={19} height={16}/>
+        <SVGIcon
+          className={styles.svgCamera}
+          {...icons.camera}
+          width={19}
+          height={16}
+        />
         <span className="sr-only">프로필 이미지 변경하기</span>
       </label>
 
       <ul className={styles.profileDetails}>
         <li>
           <span className={styles.infolabel}>닉네임</span>
-          <span className={styles.infoValue}>{userInfo.nickName || '닉네임 없음'}</span>
+          <span className={styles.infoValue}>
+            {userInfo.nickName || '닉네임 없음'}
+          </span>
         </li>
         <li>
           <span className={styles.infolabel}>관심사</span>
-          <span className={styles.infoValue}>{userInfo.interest || '관심사 없음'}</span>
+          <span className={styles.infoValue}>
+            {userInfo.interest || '관심사 없음'}
+          </span>
         </li>
         <li>
           <span className={styles.infolabel}>나이</span>
-          <span className={styles.infoValue}>{userInfo.age || '나이 정보 없음'}</span>
+          <span className={styles.infoValue}>
+            {userInfo.age || '나이 정보 없음'}
+          </span>
         </li>
         <li>
           <span className={styles.infolabel}>성별</span>
-          <span className={styles.infoValue}>{userInfo.gender || '성별 정보 없음'}</span>
+          <span className={styles.infoValue}>
+            {userInfo.gender || '성별 정보 없음'}
+          </span>
         </li>
       </ul>
-      
+
       <button
         className={styles.editButton}
         onClick={() => navigate('./settings/userInfoInput/:progress')}
