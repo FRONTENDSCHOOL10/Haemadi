207913c3aff0716f3a53a14f84158bd4999de862
JoowonJang <wndnjs5248@hanmail.net>
2024-09-24 00:53:16 +0900

    feat: settingsPage에서 프로필 이미지 변경 시 webp로 변환해서 서버에  전송

---
+++ src/utils/convertImageToWebP.js
@@ -0,0 +1,31 @@
+import { imageConverter } from 'upload-images-converter';
+import { calculateAspectRatioSize } from './calculateAspectRatioSize';
+import { getImageSize } from './getImageSize';
+
+export const convertImageToWebP = async (imageFile) => {
+  const { width: originWidth, height: originHeight } =
+    await getImageSize(imageFile);
+
+  const { width, height } = calculateAspectRatioSize({
+    originWidth,
+    originHeight,
+    maxWidthOrHeight: 1280,
+  });
+
+  const compressedBlob = await imageConverter({
+    files: [imageFile],
+    width,
+    height,
+  });
+
+  const outputWebpFile = new File(
+    [compressedBlob[0]],
+    `${Date.now().toString()}.webp`
+  );
+
+  const dataTransfer = new DataTransfer();
+
+  dataTransfer.items.add(outputWebpFile);
+
+  return dataTransfer.files;
+};
