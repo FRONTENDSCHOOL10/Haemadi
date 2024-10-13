fe39c491ae898a31c11a9a6833416d223eefae3b
jaehwan-space <ma2582m@naver.com>
2024-10-13 15:24:43 +0900

    feat: Toast 출력 개수 제한

--- src/stores/ToasterStore.js
+++ src/stores/ToasterStore.js
@@ -8,16 +8,25 @@ export const ToasterStore = create((set) => ({
       type,
       message,
     };
-    set((state) => ({
-      toasts: [...state.toasts, newToast],
-    }));
-    setTimeout(
-      () =>
-        set((state) => ({
-          toasts: state.toasts.filter((toast) => toast.id !== newToast.id),
-        })),
-      2000
-    );
+
+    set((state) => {
+      // 토스트 최대 개수를 3으로 제한
+      let updatedToasts = [...state.toasts, newToast];
+
+      if (updatedToasts.length > 3) {
+        updatedToasts.shift(); // 가장 오래된 토스트 제거
+      }
+
+      return {
+        toasts: updatedToasts,
+      };
+    });
+
+    setTimeout(() => {
+      set((state) => ({
+        toasts: state.toasts.filter((toast) => toast.id !== newToast.id),
+      }));
+    }, 2000); // 2초 후 자동 삭제
   },
   removeToast: (id) => {
     set((state) => ({
