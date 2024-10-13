37360c9a3ec2b6c59fe3cb805ef29958ae42d7fe
JoowonJang <wndnjs5248@hanmail.net>
2024-10-13 16:25:02 +0900

    feat: react-query 관리 객체 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,28 @@
 import '@/styles/main.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import ReactModal from 'react-modal';
+import App from './App';
 
 ReactModal.setAppElement('#root');
 
 const container = document.getElementById('root');
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 10 * 60 * 1000, // 10분 동안 캐시 유지
+      retry: 1, // 실패 시 한 번만 재시도
+    },
+  },
+}); // react-query 관리 객체
 
 if (container) {
   createRoot(container).render(
     <StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>
   );
 } else {
