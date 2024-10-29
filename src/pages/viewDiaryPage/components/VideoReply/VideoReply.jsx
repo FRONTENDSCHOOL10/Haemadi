f7d2fa04da98d7700a6cc24f65d5a09f5d0bcd56
JoowonJang <wndnjs5248@hanmail.net>
2024-10-29 17:53:56 +0900

    refactor: viewDiaryPage의 답장 컨텐츠 이미지를 아이콘으로 변경

--- src/pages/viewDiaryPage/components/VideoReply/VideoReply.jsx
+++ src/pages/viewDiaryPage/components/VideoReply/VideoReply.jsx
@@ -1,17 +1,24 @@
-import { string, arrayOf } from 'prop-types';
+import { string, arrayOf, bool } from 'prop-types';
 import styles from './VideoReply.module.css';
 import { memo } from 'react';
+import SVGIcon from '@/components/SVGIcon/SVGIcon';
+import icons from '@/icons';
 
 VideoReply.propTypes = {
-  imgSrc: string.isRequired,
+  desktop: bool.isRequired,
   videoTitle: string.isRequired,
   hashTags: arrayOf(string),
 };
 
-function VideoReply({ imgSrc, videoTitle, hashTags }) {
+function VideoReply({ desktop, videoTitle, hashTags }) {
   return (
     <figure className={styles.component}>
-      <img src={imgSrc} alt={`${videoTitle} 썸네일`} loading="lazy" />
+      <SVGIcon
+        {...icons.video}
+        width={desktop ? 52 : 42}
+        height={desktop ? 33 : 27}
+      />
+
       <figcaption>
         <strong>{videoTitle}</strong>
         <ul>
